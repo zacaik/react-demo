b13a84d741dd14246c33ef6103fdb6211bb30d4f
lijunran <646319370@qq.com>
2023-08-31 00:44:33 +0800

    feat: 项目初始化

---
+++ react-review/src/App.js
@@ -0,0 +1,14 @@
+/*
+ * @Author: lijunran 646319370@qq.com
+ * @Date: 2023-08-30 23:26:42
+ * @LastEditors: lijunran 646319370@qq.com
+ * @LastEditTime: 2023-08-31 00:06:49
+ * @Description:
+ */
+import "./App.css";
+
+function App() {
+  return <div className="App">hello</div>;
+}
+
+export default App;
---
+++ react-review/src/index.js
@@ -0,0 +1,14 @@
+/*
+ * @Author: lijunran 646319370@qq.com
+ * @Date: 2023-08-30 23:26:42
+ * @LastEditors: lijunran 646319370@qq.com
+ * @LastEditTime: 2023-08-31 00:06:12
+ * @Description:
+ */
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
